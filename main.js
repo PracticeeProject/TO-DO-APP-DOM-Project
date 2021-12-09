f206363e30c6dda6572e7dbcdd11ad35deaf9a47
pranta <siamonpranta@gmail.com>
2021-12-09 17:06:35 +0600

    Connecting to GitHub

--- main.js
+++ main.js
@@ -1,19 +1,71 @@
+// Select element and assign them in variables
 
-// =================================================
-//  কিভাবে Array এর মধ্যে একটি Letter এর Position 
-//  নির্ণয় করা যায় ।।
-// =================================================
+let form = document.querySelector('form');
+let inputBox = document.querySelector('.inputBox');
+let incompliteUl = document.querySelector('.incomUl');
+let compliteUl = document.querySelector('.complite_task ul');
 
-function Alphabet( arry , value ){
-    let length = arry.length;
-    for(let i = 0 ; i < length ; i++) {
-        if( arry[i] === value ){
-            return i = i + 1;
-        }
-    }
-    return "NOT FOUND!";
-};
+// function 
 
-console.log(Alphabet(["g","b","c","a","o","x","w"],"c"));
+let createTask = function(task){
+    let taskList = document.createElement("li");
+    let label = document.createElement("label");
+    let checkBox = document.createElement("input");
 
+    checkBox.type = "checkBox";
 
+    label.innerText =task;
+    taskList.appendChild(checkBox);
+    taskList.appendChild(label);
+
+    return taskList;
+
+}
+let addTask = function(event){
+    event.preventDefault();
+
+    let taskList =createTask(inputBox.value);
+    incompliteUl.appendChild(taskList);
+    inputBox.value = "";
+
+    incompliteTask(taskList, compliteTask)
+    
+}
+let deletebtn = function(list, deleteList){
+    let liList = list;
+    let deleteBtnn = liList.querySelector('input[type="submit"]');
+    deleteBtnn.onclick = deleteList;
+}
+let deleteMe = function(){
+    let listOf = this.parentNode;
+    listOf.remove();
+}
+let compliteTask = function(){
+    let taskList = this.parentNode;
+    let checkBox = taskList.querySelector('input[type="checkbox"]');
+    let deltes = document.createElement("input");
+    deltes.type = "submit";
+    deltes.value = "Delete";
+    taskList.appendChild(deltes);
+
+    checkBox.remove();
+    compliteUl.appendChild(taskList);
+    deletebtn(taskList, deleteMe);
+
+}
+
+let incompliteTask = function(listItem, clickIt){
+    let checkBox = listItem.querySelector('input[type="checkBox"]');
+    checkBox.onchange = clickIt;
+}
+
+for( let i=0; i<incompliteUl.children.length; i++){
+    incompliteTask(incompliteUl.children[i], compliteTask);
+}
+for( let i=0; i<compliteUl.children.length; i++){
+    deletebtn(compliteUl.children[i], deleteMe);
+}
+
+// Functin Call
+
+form.addEventListener("submit", addTask);
